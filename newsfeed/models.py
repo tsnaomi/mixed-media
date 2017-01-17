74d8f3a9c52a3b3c6044064efcb64e3af9e203f2
Naomi T. S <tsnaomi@uw.edu>
2017-01-17 00:57:39 -0800

    add basic models

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -1,5 +1,33 @@
-from __future__ import unicode_literals
+import re
 
+from django.contrib.auth.models import User as AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# TODO: add managers, set up unique fields, default query orders
+
+class Hipster(AbstractUser):
+    pass
+
+
+class Collection(models.Model):
+    hipster = models.ForeignKey(Hipster)
+    title = models.CharField(max_length=100, default='Untitled')
+    title_slug = models.SlugField()
+    is_public = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # on save, populate title_slut with a normalized version of title
+        self.title_slug = '-'.join(re.split(r'[^A-Z0-9]+', self.title.lower()))
+
+        super(Collection, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    hipster = models.ForeignKey(Hipster)
+    caption = models.CharField(max_length=100, default='Untitled')
+    collection = models.ForeignKey(Collection)
+    api = models.BooleanField()     # the API of the post (e.g., 'SoundCloud')
+    source = models.URLField()      # the URL of the API resource
+    date = models.DateTimeField(auto_now_add=True)
